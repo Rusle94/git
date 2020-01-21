6d38b647a924e7ed5dde0f04e7d631eebc5e426e
Rusle94 <57627487+Rusle94@users.noreply.github.com>
2020-01-21 07:24:03 +0100

    w

---
+++ paddle.js
@@ -0,0 +1,21 @@
+
+export default class Paddle {
+constructor (gameWidth,gameHeight) {
+this.width = 150;
+this.height = 30;
+this.position = {
+    x: gameWidth /2- this.width/ 2,
+    y: gameHeight - this.height - 10
+}
+}
+
+
+draw(ctx){
+ctx.fillStyle = '#ff';
+ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+
+}
+
+
+}
